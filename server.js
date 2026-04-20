60fa4684f774068bfb5fc7cdd6d61adace5ebb73
CHANDRUKUMAR C <chandrukumar4527@gmail.com>
2026-04-20 13:45:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://shopsoforntend.netlify.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
