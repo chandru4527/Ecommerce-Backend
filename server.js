23ec657df7ab620d7f80eb37fed671e6530618dc
CHANDRUKUMAR C <chandrukumar4527@gmail.com>
2026-04-20 13:58:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors({
   origin: [
     "https://chandru4527.github.io/Ecommerce-Frontend/",
     "https://shopsoforntend.netlify.app"
-  ],,
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
