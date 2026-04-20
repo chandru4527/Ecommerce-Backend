a8430a25cd876d6532d77bb57db8b3b9c34004e0
CHANDRUKUMAR C <chandrukumar4527@gmail.com>
2026-04-20 12:26:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors({
   origin: [
-  "http://localhost:5173"
+  "https://ecommerce-backend-hbly.onrender.com"
   ],
   credentials: true,
   exposedHeaders: ["set-cookie"]
